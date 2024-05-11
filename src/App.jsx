b041aa3e665be6182ab5ee5ae4bbcedcd256453e
Beksi <heliumvalley@gmail.com>
2024-05-11 20:45:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Info from './components/Info'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Info />
+      <h2>fdsf</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+        <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, facere eaque! Quibusdam illo consequatur tenetur expedita sint distinctio. Totam, ipsum </h2>
+    </div>
+  )
+}
+
+export default Info
